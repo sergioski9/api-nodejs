785dee9522428c1700f6cf6069ab2eca1bd27b84
sergio andres <rodascruzsergio@gmail.com>
2022-11-16 15:25:01 -0400

    modify api in the user routes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const dataUsers = require('./data/user')
 
 const app = express()
 
--- routes/routesUser.js
+++ routes/routesUser.js
@@ -19,4 +19,32 @@ routingUser.get('/:id', (req, res) => {
   res.status(404).send('No se encontro ningun resultado')
 })
 
+routingUser.post('/', (req, res) => {
+  const userInfoForAdd = req.body
+
+  if (userInfoForAdd) {
+    dataUsers.push(userInfoForAdd)
+    res.send(dataUsers)
+  }
+
+  res.status(400).send('Datos incorrectos o informacion no recibida')
+})
+
+routingUser.put('/:id', (req, res) => {
+  const id = req.params.id
+  const bodyReq = req.body
+
+  const indexUserUpdate = dataUsers.findIndex((curso) => curso.id == id)
+
+  if (indexUserUpdate >= 0) {
+    const currentUser = dataUsers[indexUserUpdate]
+
+    Object.assign(currentUser, bodyReq)
+    res.send(dataUsers)
+  }
+
+  res.status(400).send('No se pudo realizar la actualizacion debido a un error de datos o del servidor...')
+})
+
+
 module.exports = routingUser
