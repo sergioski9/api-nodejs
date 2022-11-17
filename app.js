4400e9b18343b7b490ccbc0787aee9cefe31eebf
sergio andres <rodascruzsergio@gmail.com>
2022-11-17 08:22:30 -0400

    modify the home

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const app = express()
 
 // Routing
@@ -12,7 +11,30 @@ app.use('/api/v1/cursos', routingCursos)
 // Home the APIRESTful
 
 app.get('/', (req, res) => {
-  res.send('Mi primer servidor con nodejs y express')
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <main>
+        <article>
+          <h1>
+            Welcome in the API RESTful created by Sergio Rodas Cruz
+          </h1>
+          <h3>
+            this page is the home API, for more information visit in the
+            /api/v1/users or /api/v1/cursos
+          </h3>
+        </article>
+      </main>
+    </body>
+  </html>
+`)
 })
 
 // PORT
